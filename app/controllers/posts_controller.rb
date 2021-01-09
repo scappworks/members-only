b21f7f53041943c51384dd3680f6022088a9413f
scott <scappworks@gmail.com>
2021-01-09 03:51:13 -0500

    add new and create methods to posts controller. add new post form to posts index page. add posts to routes resources

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
