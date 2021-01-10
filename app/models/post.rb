126c1248620bd7df47cc75c9f995e07089e3c31d
scott <scappworks@gmail.com>
2021-01-10 01:03:50 -0500

    add character limits to title and body in posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
     validates :title, :body, :user_id, presence: true
+    validates_length_of :title, maximum: 40, allow_blank: false
+    validates_length_of :body, maximum: 50, allow_blank: false
+
 
     belongs_to :user
 end
