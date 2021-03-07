01b598b31e39243a3cba7c89c89f7303a5205edd
scott <scappworks@gmail.com>
2021-03-07 01:12:22 -0500

    seed new database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ :username => "usertwo", :email => "usertwo@email.com", :password => "password" },
+    { :username => "userthree", :email => "userthree@email.com", :password => "password" },
+    { :username => "userfive", :email => "userfive@email.com", :password => "password" },
+    { :username => "userfive", :email => "userfive@email.com", :password => "password" }])
+
+posts = Post.create([{ :title => "post one", :body => "post one body", :user_id => 1 },
+    { :title => "post two", :body => "post two body", :user_id => 1 },
+    { :title => "post three", :body => "post three body", :user_id => 2 },
+    { :title => "post four", :body => "post four body", :user_id => 3 },
+    { :title => "post five", :body => "post five body", :user_id => 4 },
+    { :title => "post six", :body => "post six body", :user_id => 5 },
+    { :title => "post seven", :body => "post seven body", :user_id => 1 }])
